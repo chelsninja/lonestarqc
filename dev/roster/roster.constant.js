a0a49db8b18bbdfc87056343bf415284f6a87d0c
cmlorenz <cmlorenz@syr.edu>
2015-10-01 09:24:51 -0500

    phase 2.9 - added two columns to roster

--- dev/roster/roster.constant.js
+++ dev/roster/roster.constant.js
@@ -8,182 +8,235 @@ angular.module('app')
                 jersey: 25,
                 position: 'Chaser',
                 hometown: 'New York, NY',
-                yearsOfExp: 1
+                experience: 1
             },
             {
                 firstName: 'Becca',
                 lastName: 'DuPont',
                 headShot: 'dupont.jpg',
                 jersey: 33,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Mansfield, TX',
+                experience: 5
             },
             {
                 firstName: 'Stephen',
                 lastName: 'Bell',
                 headShot: 'bell.jpg',
                 jersey: 3,
-                position: 'Keeper'
+                position: 'Keeper',
+                hometown: 'Austin, TX',
+                experience: 3.5
             },
             {
                 firstName: 'Drew',
                 lastName: 'Wasikowski',
                 headShot: 'd-wasikowski.jpg',
                 jersey: 16,
-                position: 'Chaser/Keeper'
+                position: 'Chaser / Keeper',
+                hometown: 'Keller, TX',
+                experience: 5
             },
             {
                 firstName: 'Chris',
                 lastName: 'Scholz',
                 headShot: 'scholz.jpg',
                 jersey: 7,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: '-',
+                experience: '-'
             },
             {
                 firstName: 'Joshua',
                 lastName: 'Tates',
                 headShot: 'tates.jpg',
-                jersey: 80,
-                position: 'Seeker/Chaser'
+                jersey: 23,
+                position: 'Seeker / Chaser',
+                hometown: 'Garland, TX',
+                experience: 1
             },
             {
                 firstName: 'Simon',
                 lastName: 'Arends',
                 headShot: 'arends.jpg',
                 jersey: 20,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'League City, TX',
+                experience: 3
             },
             {
                 firstName: 'Kody',
                 lastName: 'Marshall',
                 headShot: 'marshall.jpg',
                 jersey: 1,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Austin, TX',
+                experience: 5
             },
             {
                 firstName: 'Mathieu',
                 lastName: 'Gregorie',
                 headShot: 'gregorie.jpg',
                 jersey: 2,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Plano, TX',
+                experience: 6
             },
             {
                 firstName: 'Kedzie',
                 lastName: 'Teller',
                 headShot: 'teller.jpg',
                 jersey: 10,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Boston, MA',
+                experience: 6
             },
             {
                 firstName: 'Luke',
                 lastName: 'Langlinais',
                 headShot: 'langlinais.jpg',
                 jersey: 15,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'San Antonio, TX',
+                experience: 3
             },
             {
                 firstName: 'Joe',
                 lastName: 'Wright',
                 headShot: 'wright.jpg',
                 jersey: 4,
-                position: 'Chaser/Beater'
+                position: 'Chaser / Beater',
+                hometown: '-',
+                experience: '-'
             },
             {
                 firstName: 'Sam',
                 lastName: 'Haimowitz',
                 headShot: 'haimowitz.jpg',
                 jersey: 6,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: '-',
+                experience: 2
             },
             {
                 firstName: 'Nichole',
                 lastName: 'Wasikowski',
                 headShot: 'n-wasikowski.jpg',
                 jersey: 14,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Austin, TX',
+                experience: '-'
             },
             {
                 firstName: 'Josh',
                 lastName: 'Carroll',
                 headShot: 'carroll.jpg',
                 jersey: 19,
-                position: 'Beater/Seeker'
+                position: 'Beater / Seeker',
+                hometown: 'Lubbock, TX',
+                experience: 6
             },
             {
                 firstName: 'Ethan',
                 lastName: 'Banner',
                 headShot: 'banner.jpg',
                 jersey: 44,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Austin, TX',
+                experience: 2
             },
             {
                 firstName: 'Carlos',
                 lastName: 'Elarba',
                 headShot: 'elarba.jpg',
                 jersey: 18,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: '-',
+                experience: '-'
             },
             {
                 firstName: 'Savannah',
+                nickname: 'Savvy',
                 lastName: 'Allison',
                 headShot: 'allison.jpg',
                 jersey: 13,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Houston, TX',
+                experience: 6
             },
             {
                 firstName: 'Sarah',
                 lastName: 'Holub',
                 headShot: 'holub.jpg',
                 jersey: 11,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Sealy, TX',
+                experience: 5
             },
             {
                 firstName: 'Hope',
                 lastName: 'Machala',
                 headShot: 'machala.jpg',
                 jersey: 8,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Sealy, TX',
+                experience: 5
             },
             {
                 firstName: 'Jonathon',
                 lastName: 'Ruland',
                 headShot: 'ruland.jpg',
                 jersey: 5,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Madison, WI',
+                experience: 1.5
             },
             {
                 firstName: 'Craig',
                 lastName: 'Garrison',
                 headShot: 'garrison.jpg',
                 jersey: 0,
-                position: 'Chaser/Keeper'
+                position: 'Chaser / Keeper',
+                hometown: 'Austin, TX',
+                experience: 4
             },
             {
                 firstName: 'Paxton',
                 lastName: 'Casey',
                 headShot: 'casey.jpg',
                 jersey: 24,
-                position: 'Chaser/Beater'
+                position: 'Chaser / Beater',
+                hometown: 'Crosby, TX',
+                experience: 3.5
             },
             {
                 firstName: 'Lyz',
+                nickname: 'Lyzard',
                 lastName: 'Reblin',
                 headShot: 'reblin.jpg',
                 jersey: 99,
-                position: 'Chaser'
+                position: 'Chaser',
+                hometown: 'Sugar Land, TX',
+                experience: 3
             },
             {
                 firstName: 'Colton',
                 lastName: 'Mikulencak',
                 headShot: 'mikulencak.jpg',
                 jersey: 34,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Thrall, TX',
+                experience: 1
             },
             {
                 firstName: 'Brittany',
+                nickname: 'B-Money',
                 lastName: 'Munden',
                 headShot: 'munden.jpg',
                 jersey: 29,
-                position: 'Beater'
+                position: 'Beater',
+                hometown: 'Austin, TX',
+                experience: 3
             }
         ]
     });
